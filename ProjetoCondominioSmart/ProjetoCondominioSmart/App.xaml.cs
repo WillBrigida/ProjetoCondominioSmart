12d3c787c4402d952233bb656e2c781fa574629c
will brigida <will_brigida@hotmail.com>
2019-09-19 20:57:26 -0300

    Atualização App Center

--- ProjetoCondominioSmart/ProjetoCondominioSmart/App.xaml.cs
+++ ProjetoCondominioSmart/ProjetoCondominioSmart/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Prism;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Prism;
 using Prism.DryIoc;
 using Prism.Ioc;
 using Prism.Plugin.Popups;
@@ -13,18 +16,21 @@ namespace ProjetoCondominioSmart
     public partial class App : PrismApplication
     {
         public App() : this(null) { }
-      
-        public App(IPlatformInitializer initializer): this(initializer, true) { }
-      
+
+        public App(IPlatformInitializer initializer) : this(initializer, true) { }
+
         public App(IPlatformInitializer initializer, bool setFormsDependencyResolver) : base(initializer, setFormsDependencyResolver) { }
 
         protected override async void OnInitialized()
         {
             InitializeComponent();
-//#if DEBUG
-//            HotReloader.Current.Run(this);
+            //#if DEBUG
+            //            HotReloader.Current.Run(this);
 
             await NavigationService.NavigateAsync("LoginPage");
+
+            AppCenter.Start("android=c2f894c9-c22c-4e95-965e-8365c6a82c65;",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
