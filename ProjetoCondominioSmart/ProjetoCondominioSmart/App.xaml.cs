0fd8b853ce39f94e7646011fa2c696b99f35fefd
will brigida <will_brigida@hotmail.com>
2019-09-24 16:56:11 -0300

    Atualização

--- ProjetoCondominioSmart/ProjetoCondominioSmart/App.xaml.cs
+++ ProjetoCondominioSmart/ProjetoCondominioSmart/App.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using Com.OneSignal;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using Prism;
 using Prism.DryIoc;
 using Prism.Ioc;
@@ -7,6 +10,7 @@ using ProjetoCondominioSmart.ViewModels;
 using ProjetoCondominioSmart.ViewModels.Popup;
 using ProjetoCondominioSmart.Views;
 using ProjetoCondominioSmart.Views.Popup;
+using System.Collections.Generic;
 using Xamarin.Forms;
 
 namespace ProjetoCondominioSmart
@@ -14,9 +18,9 @@ namespace ProjetoCondominioSmart
     public partial class App : PrismApplication
     {
         public App() : this(null) { }
-      
-        public App(IPlatformInitializer initializer): this(initializer, true) { }
-      
+
+        public App(IPlatformInitializer initializer) : this(initializer, true) { }
+
         public App(IPlatformInitializer initializer, bool setFormsDependencyResolver) : base(initializer, setFormsDependencyResolver) { }
 
         protected override async void OnInitialized()
@@ -30,6 +34,25 @@ namespace ProjetoCondominioSmart
 
             OneSignal.Current.StartInit("5bd3b9d2-1a0c-417d-89d1-3a3d9fb10cfc")
                  .EndInit();
+
+            AppCenter.Start("android=d1466097-4979-46a5-aba1-9ee42a8ccb6b;",
+                  typeof(Analytics), typeof(Crashes));
+
+
+            try
+            {
+                Crashes.GenerateTestCrash();
+            }
+            catch (System.Exception exception)
+            {
+
+                var properties = new Dictionary<string, string>
+                {
+                    { "Category", "Music" },
+                    { "Wifi", "On" }
+                };
+                Crashes.TrackError(exception, properties);
+            }
         }
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
